44d4186b22f08c511c0e54f8cf77b0e1b1d6ddd5
Burman Bedford <burman@Burmans-Air-2122.lan>
2023-04-12 16:47:08 -0400

    saving my recent changes then cloning down for my own branch

---
+++ Front-End/turo/src/BodyComponents/carImages/heartComponent.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+import Heart from 'react-heart'
+
+
+export function HeartIcon() {
+//     const [active, setActive] = useState(false)
+//     return (
+//         <div style={{ width: "2rem" }}>
+//           <Heart className='heart-icon' isActive={active} onClick={() => setActive(!active)} />
+//         </div>
+//     );
+//   }
+
+    const [active, setActive] = useState(false)
+    return (
+        <div style={{ width: "2rem" }}>
+          <Heart className='heart-icon' isActive={active} onClick={() => setActive(!active)} style = {{fill: active ? "transparent" : "red", stroke: active ? "black":"transparent"}} />
+        </div>
+    );
+  }
--- Front-End/turo/src/BodyComponents/carImages/user1.jsx
+++ Front-End/turo/src/BodyComponents/carImages/user1.jsx
@@ -2,24 +2,28 @@ import React, {useState} from "react";
 import { SliderData } from "./sliderdata";
 import {CgChevronRight, CgChevronLeft} from 'react-icons/cg'
 import './image.css'
+import { HeartIcon } from "./heartComponent";
+import './heartIcon.css'
+import { NumberList } from "./numberList";
 
 const ImageSlider = ({ slides }) => {
     const [current, setCurrent] = useState(0);
-  
+
     const nextSlide = () => {
       setCurrent(current === slides.length - 1 ? 0 : current + 1);
     };
-  
+
     const prevSlide = () => {
       setCurrent(current === 0 ? slides.length - 1 : current - 1);
     };
-  
+
     if (!Array.isArray(slides) || slides.length <= 0) {
       return null;
     }
   
     return (
       <section className='slider'>
+        <HeartIcon className='heart-icon'/>
         <CgChevronLeft className='left-arrow' onClick={prevSlide} />
         <CgChevronRight className='right-arrow' onClick={nextSlide} />
         {SliderData.map((slide, index) => {
@@ -29,7 +33,7 @@ const ImageSlider = ({ slides }) => {
               key={index}
             >
               {index === current && (
-                <img src={slide.image} alt='travel image' className='image' />
+                <img src={slide.image} alt='Jeep' className='image' />
               )}
             </div>
           );
