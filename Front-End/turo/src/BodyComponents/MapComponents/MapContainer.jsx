88851d8dff54d17c26b9984ff39a4a1573cb6373
Mason Money <moneymason0@gmail.com>
2023-04-12 13:21:56 -0600

    Got pin on google map and finalized css of locations box

--- Front-End/turo/src/BodyComponents/MapComponents/MapContainer.jsx
+++ Front-End/turo/src/BodyComponents/MapComponents/MapContainer.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-import LocationsPin from './LocationsPin';
-import './MapContainer.css';
+import Circle from 'google-map-react';
 import LocationsDisplay from './LocationsDisplay';
-
+import './MapContainer.css';
 
 export default function Map({zoomLevel}) {
 
-  const location = {
-    address: '1600 Amphitheatre Parkway, Mountain View, california.',
-    center: {lat: 37.712070, lng: -122.218385}    
-  }
-
-  const points = [
-    { id: 1, title: "Round Pond", lat: 37.712070, lng: -122.218385},
-    { id: 2, title: "Round Pond", lat: 37.621177, lng: -122.378569},
-    { id: 3, title: "Round Pond", lat: 37.712070, lng: -122.7}
-  ];
+  let center = {
+    lat: 37.712070,
+    lng: -122.218385
+  };
 
   return (
     <div className="map">
@@ -24,21 +17,19 @@ export default function Map({zoomLevel}) {
         <div className="google-map">
         <GoogleMapReact
         bootstrapURLKeys={{ key: "AIzaSyAf_3u5iBWiCgqsX5B2ue03Bu7UZxosVco", language: "en", region: "US" }}
-        defaultCenter={location.center}
+        defaultCenter={center}
         defaultZoom={zoomLevel}
+        yesIWantToUseGoogleMapApiInternals={true}
+        onGoogleApiLoaded={({map, maps}) =>
+          new maps.Circle({
+            strokeOpacity: 0,
+            fillColor: '#0000FF',
+            fillOpacity: 0.3,
+            map,
+            center: {lat: center.lat, lng: center.lng},
+            radius: 250,
+          })}
         >
-        {points.map(({ lat, lng, id }) => {
-            console.log(lat);
-            console.log(lng);
-          return (
-            <LocationsPin
-                key={id}
-                lat={lat}
-                lng={lng}
-                text={id}
-            />
-          );
-        })}
         </GoogleMapReact>
       </div>
     </div>
