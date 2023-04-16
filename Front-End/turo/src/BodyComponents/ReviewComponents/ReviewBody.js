cf1a081eec3e11dbafc93c665dde4086d58d56c5
crose275 <crose275@gmail.com>
2023-04-16 12:57:07 -0700

    updated reviews

--- Front-End/turo/src/BodyComponents/ReviewComponents/ReviewBody.js
+++ Front-End/turo/src/BodyComponents/ReviewComponents/ReviewBody.js
@@ -1,13 +1,23 @@
-import React, {useContext, useState} from 'react'
+import React, {useContext, useEffect, useState} from 'react'
 import { ReviewContext } from '../../context/ReviewsContext'
 import  Button  from 'react-bootstrap/button'
 import UserReview from './UserReview'
 
 export default function ReviewBody() {
     const reviews = useContext(ReviewContext)
-    console.log(reviews)
-    const [activeReviews, setActiveReviews] = useState(reviews.slice(0, 5));
+    console.log("reviews: "  + reviews)
     const [numToShow, setNumToShow] = useState(5);
+    const [activeReviews, setActiveReviews] = useState([null]);
+    let flag = false
+    
+    useEffect(()=>{
+        setActiveReviews(reviews.slice(0, numToShow))
+        console.log("active reviews: " + activeReviews)
+    }, [reviews])
+
+  
+    
+
 
     const showMoreReviews = () => {
         const newNumToShow = numToShow + 5;
@@ -18,9 +28,13 @@ export default function ReviewBody() {
 
     return (
         <div>
+            {console.log(activeReviews)}
             <p style={{fontSize: '12px', fontWeight: 900, color: '#767677'}}>Reviews</p>
-            <UserReview />
-            < Button variant='primary' />
+            {activeReviews.map((review)=>{  
+               {console.log(review)} 
+            return <UserReview review={review}/>
+        })}
+            < Button variant='primary' onClick={showMoreReviews}>See more</Button>
         </div>
     )
 }
--- Front-End/turo/src/BodyComponents/ReviewComponents/UserReview.js
+++ Front-End/turo/src/BodyComponents/ReviewComponents/UserReview.js
@@ -1,20 +1,20 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import {ReactComponent as Star} from './asset 60.svg'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { ReviewContext } from '../../context/ReviewsContext'
 
 
-export default function UserReview() {
-    const reviews = useContext(ReviewContext)
-    console.log(reviews)
-    const review = useState(reviews[0])
-    console.log(review)
-    const rating = review.review_rating
+export default function UserReview({review}) {
+    const [rating, setRating] = useState(review.review_rating)
+    const [ratingArray, setRatingArray]= useState(Array.from({length: rating}, (v, i) => i))
     console.log(rating)
     // creates an array of numbers with length of rating. 
-    const ratingArray = Array.from({length: rating}, (v, i) => i);
-    console.log(ratingArray)
+    // useEffect(()=>{
+    //     setRatingArray(Array.from({length: rating}, (v, i) => i));
+    //     console.log(ratingArray)
+    // })
+
 
     return (
         <div className='d-flex align-content-center' style={{width: '100%', border: "1px solid black", borderBottom: '1px solid  #E7E7E8'}}>
