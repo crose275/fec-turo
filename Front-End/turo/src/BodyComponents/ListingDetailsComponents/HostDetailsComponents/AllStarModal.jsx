09ccc0e540a90197ea63908f37d65f8953c0fc91
Mason Money <moneymason0@gmail.com>
2023-04-17 08:50:06 -0600

    Finalied Listing component

--- Front-End/turo/src/BodyComponents/ListingDetailsComponents/HostDetailsComponents/AllStarModal.jsx
+++ Front-End/turo/src/BodyComponents/ListingDetailsComponents/HostDetailsComponents/AllStarModal.jsx
@@ -6,14 +6,14 @@ export default function AllStarModal({toggleAllStarModal}) {
     <div className='AllStarModal'>
       <span className="closeButton" onClick={toggleAllStarModal}> X </span>
       <div className='ImageContainer'>
-        <img className='AllStarBadgeIcon' src='https://images.squarespace-cdn.com/content/v1/5e83804992ec7778163264a4/1588468065560-K2Z25B36LE74E9J4URUA/All-star-host-badge.png'></img>
+        <img className='AllStarBadgeIcon' src='https://images.squarespace-cdn.com/content/v1/5e83804992ec7778163264a4/1588468065560-K2Z25B36LE74E9J4URUA/All-star-host-badge.png' alt="All-Star Badge"></img>
       </div>
       <div className='Content'>
         <h2 className="AllStarHeader">All-Star Hosts</h2>
         <span className='AllStarDescription'>
             All-Star Hosts are the most experienced and responsive hosts on Turo. These outstanding hosts have completed at least 10 trips in the last year, and consistently go the extra mile, earning excellent reviews from guests.
         </span>
-      </div>
+      </div> 
     </div>
   )
 }
--- Front-End/turo/src/FooterComponents/FooterMainComponents/TopHalfComponents/SocialMediaComponents/SocialMediaIcons.jsx
+++ Front-End/turo/src/FooterComponents/FooterMainComponents/TopHalfComponents/SocialMediaComponents/SocialMediaIcons.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import './SocialMediaIcons.css'
+import './SocialMediaIcons.css';
+import {ReactComponent as Facebook} from './Facebook.svg';
+import {ReactComponent as Twitter} from './Twitter.svg';
+import {ReactComponent as Instagram} from './Instagram.svg';
+import {ReactComponent as YouTube} from './YouTube.svg';
+
 
 export default function SocialMediaIcons() {
 
-    const img1 ="https://freepngimg.com/thumb/facebook/62588-and-icons-facebook-computer-black-logo-white.png"
-    const img2 ="https://cdn-icons-png.flaticon.com/512/0/654.png"
-    const img3 = "https://assets.stickpng.com/thumbs/5ecec78673e4440004f09e77.png"
-    const img4 = "https://www.kindpng.com/picc/m/21-210198_youtube-play-button-computer-icons-black-and-white.png"
-
   return (
     <div className='SocialMediaIconsContainer'>
-      <img className='SocialMediaIcons' src={img1}></img>
-      <img className='SocialMediaIcons' src={img2}></img>
-      <img className='SocialMediaIcons' src={img3}></img>
-      <img className='SocialMediaIcons' src={img4}></img>
+      <Facebook className='SocialMediaIcons'/>
+      <Twitter className='SocialMediaIcons'/>
+      <Instagram className='SocialMediaIcons'/>
+      <YouTube className='SocialMediaIcons'/>
       <div className='blogIconContainer'>
         <span className='blogIcon'> BLOG </span>
       </div>
