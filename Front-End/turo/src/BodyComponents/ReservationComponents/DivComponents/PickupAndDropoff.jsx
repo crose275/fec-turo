0111bf496b0868ce88dc5d1790524c8dbcdf44e1
Burman Bedford <burman@Burmans-Air-2399.lan>
2023-04-17 02:10:09 -0400

    Still have to finish the calender and the price per day

--- Front-End/turo/src/BodyComponents/ReservationComponents/DivComponents/PickupAndDropoff.jsx
+++ Front-End/turo/src/BodyComponents/ReservationComponents/DivComponents/PickupAndDropoff.jsx
@@ -1,9 +1,121 @@
 import React from "react";
+import "../DivComponentsCss/PickupAndDropoff.css"
+import Dropdown from 'react-bootstrap/Dropdown'
+import { IoCarOutline } from "react-icons/io5";
+import { IoAirplaneOutline } from "react-icons/io5";
+import { FiMapPin } from "react-icons/fi";
 
 export default function PickupAndDropoff() {
     return(
-        <div>
-            Pickup & return location
+        <div id="PickupAndDropoff-container">
+            <div>
+                <label id="MainPickup">Pick up and return location</label>
+                <Dropdown >
+                    <Dropdown.Toggle id="toggle" variant="success">
+                        Oakland International Airport
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu id="menu">
+                        <p id="pickup2">
+                            PICKUP AT CAR LOCATION
+                        </p>
+                        <Dropdown.Item id="item">
+                           <button className="buttons">
+                            <span className="logo">
+                                <IoCarOutline/>
+                            </span>
+                            <span>
+                                <div id="row1">
+                                    Oakland, CA 94621
+                                </div>
+                                <div id="row2">
+                                    Free
+                                </div>
+                            </span>
+                           </button>
+                            </Dropdown.Item>
+                            <div id="message">
+                                We'll send you the exact address once your trip is booked.
+                            </div>
+                            <div id="pickup2">
+                                PICKUP LOCATION
+                            </div>
+                            <Dropdown.Item id="item">
+                                <button className="buttons1">
+                                    <span className="logo">
+                                        <IoAirplaneOutline/>
+                                    </span>
+                                    <span>
+                                        <div id="Row1">
+                                            San Francisco International Airport 
+                                        </div>
+                                        <div id="Row2">
+                                            Airport 
+                                        </div>
+                                        <div id="Row3">
+                                            $75
+                                        </div>
+                                    </span>
+                            </button>
+                            </Dropdown.Item>
+                            <Dropdown.Item id="item">
+                            <button className="buttons1">
+                                    <span className="logo">
+                                        <IoAirplaneOutline/>
+                                    </span>
+                                    <span>
+                                        <div id="Row1">
+                                            Oakland International Airport 
+                                        </div>
+                                        <div id="Row2">
+                                            Airport
+                                        </div>
+                                        <div id="Row3">
+                                            $50
+                                        </div>
+                                    </span>
+                            </button>
+                            </Dropdown.Item>
+                            <div id="pickup2">
+                                delivered to you
+                            </div>
+                            {/* <Dropdown.Divider autoClose={false}/> */}
+                            <Dropdown.Item id="item" >
+                                <button className="buttons" >
+                                    <span className="logo">
+                                        <FiMapPin/>
+                                    </span>
+                                    <span>
+                                        <div id="row1">
+                                            <input type="location" placeholder="City, airport, address"/>
+                                            
+                                        </div>
+                                        <div id="row2">
+                                            $120
+                                        </div>
+                                    </span>
+                            </button>
+                            </Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+            
+                
+                
+                
+                {/* <button 
+                aria-haspopup="listbox" 
+                aria-controls="locations" 
+                aria-expanded="false" 
+                aria-label="Location Picker"
+                role="combobox"
+                className="locationBox"
+                >
+                    <option value="location">texas</option>
+                    <p>
+                        Oakland International Airport
+                    </p>
+                </button> */}
+            </div>
         </div>
     )    
 }
--- Front-End/turo/src/BodyComponents/ReviewComponents/ReviewBody.js
+++ Front-End/turo/src/BodyComponents/ReviewComponents/ReviewBody.js
@@ -4,7 +4,8 @@ import  Button  from 'react-bootstrap/button'
 import UserReview from './UserReview'
 
 export default function ReviewBody() {
-    const reviews = useContext(ReviewContext)
+    const ReviewsContext = useContext(ReviewContext)
+    const reviews = ReviewsContext.reviews
     console.log("reviews: "  + reviews)
     const [numToShow, setNumToShow] = useState(5);
     const [activeReviews, setActiveReviews] = useState([null]);
