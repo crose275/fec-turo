052b5bc10d360d6b55274cc924f692835a70adde
crose275 <crose275@gmail.com>
2023-04-17 14:35:54 -0700

    added logic for ratings chart

--- Front-End/turo/src/BodyComponents/ReviewComponents/RatingChart.js
+++ Front-End/turo/src/BodyComponents/ReviewComponents/RatingChart.js
@@ -1,22 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import RatingBar from './RatingBar';
-// import Reviews from './Reviews';
-import ratings from './ratingData'
+
 
 export default function RatingChart({reviews}){
     const [cleanlinessAverage, setCleanlinessAverage] = useState(0)
+    const [maintenanceAverage, setMaintenanceAverage] = useState(0)
+    const [communicationAverage, setCommunicationAverage] = useState(0)
+    const [convenienceAverage, setConvenienceAverage] = useState(0)
+    const [accuracyAverage, setAccuracyAverage] = useState(0)
+    const [ratings, setRatings] = useState([])
 
     useEffect(() => {
-      const sum = reviews.reduce((acc, review) => acc + review.review_rating_cleanliness, 0);
-      const average = sum / reviews.length;
-      setCleanlinessAverage(average);
+      const sum1 = reviews.reduce((acc, review) => acc + review.review_rating_cleanliness, 0);
+      const average1 = sum1 / reviews.length;
+      const sum2 = reviews.reduce((acc, review) => acc + review.review_rating_maintenance, 0);
+      const average2 = sum2 / reviews.length;
+      const sum3 = reviews.reduce((acc, review) => acc + review.review_rating_communication, 0);
+      const average3 = sum3 / reviews.length;
+      const sum4 = reviews.reduce((acc, review) => acc + review.review_rating_convenience, 0);
+      const average4 = sum4 / reviews.length;
+      const sum5 = reviews.reduce((acc, review) => acc + review.review_rating_accuracy, 0);
+      const average5 = sum5 / reviews.length;
+      setCleanlinessAverage(average1);
+      setMaintenanceAverage(average2);
+      setCommunicationAverage(average3);
+      setConvenienceAverage(average4);
+      setAccuracyAverage(average5)
+      setRatings([
+        {
+        id: 1,
+        category: "Cleanliness",
+        rating: average1
+      }, 
+      {
+        id: 2,
+        category: "Maintenance",
+        rating: average2
+      }, 
+      {
+        id: 3,
+        category: "Communication",
+        rating: average3
+      }, 
+      {
+        id: 4,
+        category: "Convenience",
+        rating: average4
+      }, 
+      {
+        id: 5,
+        category: "Accuracy",
+        rating: average5
+      } 
+    ])
+    
     }, [reviews]);
 
 
      
   return (
     <div className="container" style={{width: "100%"}}>
-      {console.log(cleanlinessAverage)}
+      {console.log(ratings)}
       {ratings.map((rating) => (
         <div key={rating.id} className='d-flex flex-row justify-content-between' style={{border: "1px solid black"}}>
           <span>{rating.category}</span>
