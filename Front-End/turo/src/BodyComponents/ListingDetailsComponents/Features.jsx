ec44a4f28af250a14c21bd8d82a3ec332672563d
Mason Money <moneymason0@gmail.com>
2023-04-17 20:34:17 -0600

    Fixed features icon

--- Front-End/turo/src/BodyComponents/ListingDetailsComponents/Features.jsx
+++ Front-End/turo/src/BodyComponents/ListingDetailsComponents/Features.jsx
@@ -30,7 +30,7 @@ export default function Features() {
                   {featuresDetailsColumn1.map((feature) => {
                     return(
                       <div key={feature.id} className="featureContainer">
-                        <img src="https://turoimages.s3.us-east-2.amazonaws.com/User+Profile+Pics/Christina.jpeg" alt="Feature Icon" />
+                        <img src={feature.feature_icon} alt="Feature Icon" className='FeatureIcon' />
                         <span className='featureName'>{feature.feature_description}</span>
                       </div>
                     )
@@ -40,7 +40,7 @@ export default function Features() {
                   {featuresDetailsColumn2.map((feature) => {
                       return(
                         <div key={feature.id} className="featureContainer" >
-                          <img alt="Feature Icon"></img>
+                          <img src={feature.feature_icon}alt="Feature Icon" className='FeatureIcon'></img>
                           <span className='featureName'>{feature.feature_description}</span>
                         </div>
                       )
