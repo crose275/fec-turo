6d2aca34891cd6fe38953aaf647940cd3cf90b6e
crose275 <crose275@gmail.com>
2023-04-17 17:38:50 -0700

    formatted review date

--- Front-End/turo/src/BodyComponents/MainBody.jsx
+++ Front-End/turo/src/BodyComponents/MainBody.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ListingDetailsMain from './ListingDetailsComponents/ListingDetailsMain';
 import Reservation from './ReservationComponents/ReservationComponent';
+import Reviews from './ReviewComponents/ReviewMain'
+import { ReviewProvider } from '../context/ReviewsContext'
 import './MainBody.css'
 
 export default function MainBody() {
